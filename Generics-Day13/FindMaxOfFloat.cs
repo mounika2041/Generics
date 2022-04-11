311657a5d01b44a10d929e148271394dc1531ae3
duvvamounika <mounikaduvva05@gmail.com>
2022-04-11 09:36:44 +0530

    Add MaxOfFloatUC2 branch and program

---
+++ Generics-Day13/FindMaxOfFloat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Day13
+{
+    class FindMaxOfFloat
+    {
+        public static float FindMaxValue(float first, float second, float third)
+        {
+            int res = first.CompareTo(second);
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("first is having Max value");
+                return first;
+            }
+
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("second is having Max value");
+                return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("third number is having Max Value");
+                return third;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+       
--- Generics-Day13/Program.cs
+++ Generics-Day13/Program.cs
@@ -11,8 +11,22 @@ namespace Generics_Day13
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to generics");
-            int x = 10, y = 20, z = 30;
-            Console.WriteLine("{0} is having Max value out of{1},{2},{3}", FindMaxOfInteger.FindMaxValue(x, y, z), x, y, z);
+            Console.WriteLine("please choose program number to execute");
+            Console.WriteLine("1:find the max value of integers\n2:find the max value of floats\n3:find the max value of string");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    int x = 50, y = 20, z = 30;
+                    Console.WriteLine("{0} is having Max value out of{1},{2},{3}", FindMaxOfInteger.FindMaxValue(x, y, z), x, y, z);
+                    break;
+                case 2:
+                    float a = 12.34f, b = 45.78f, c = 90.54f;
+                    Console.WriteLine("{0} is having Max value out of{1},{2},{3}", FindMaxOfFloat.FindMaxValue(a, b, c), a, b, c);
+                    break;
+                default:
+                    break;
+            }
             Console.ReadLine();
         }
     }
