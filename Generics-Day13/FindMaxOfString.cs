905893efe0006634e72c01d4f23ca497a943f3b8
duvvamounika <mounikaduvva05@gmail.com>
2022-04-11 09:40:42 +0530

    Add MaxOfStringUC3 branch and program

---
+++ Generics-Day13/FindMaxOfString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Day13
+{
+    class FindMaxOfString
+    {
+        public static String FindMaxValue(String first, String second, String third)
+        {
+            int res = first.CompareTo(second);
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("first is having Max value");
+                return first;
+            }
+
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("second is having Max value");
+                return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("third number is having Max Value");
+                return third;
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+}
--- Generics-Day13/Program.cs
+++ Generics-Day13/Program.cs
@@ -24,6 +24,10 @@ namespace Generics_Day13
                     float a = 12.34f, b = 45.78f, c = 90.54f;
                     Console.WriteLine("{0} is having Max value out of{1},{2},{3}", FindMaxOfFloat.FindMaxValue(a, b, c), a, b, c);
                     break;
+                case 3:
+                    string s1 = "abc", s2 = "abc", s3 = "zbc";
+                    Console.WriteLine("{0} is having Max value out of{1},{2},{3}", FindMaxOfString.FindMaxValue(s1, s2, s3), s1, s2, s3);
+                    break;
                 default:
                     break;
             }
