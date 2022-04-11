0dca57fcd3c2438ecc6bccca85dfbfc19baca8fa
mounika2041 <101236172+mounika2041@users.noreply.github.com>
2022-04-11 10:19:08 +0530

    Update Program.cs

--- Generics-Day13/Program.cs
+++ Generics-Day13/Program.cs
@@ -11,9 +11,31 @@ namespace Generics_Day13
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to generics");
-            int k = 15, l = 34, m = 90;
-            new GenericsFindMax<int>(k, l, m).TestMaximum();
+            Console.WriteLine("please choose program number to execute");
+            Console.WriteLine("1:find the max value of integers\n2:find the max value of floats\n3:find the max value of string\n4:Find  Max using Generic Class");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    int x = 50, y = 20, z = 30;
+                    Console.WriteLine("{0} is having Max value out of{1},{2},{3}", FindMaxOfInteger.FindMaxValue(x, y, z), x, y, z);
+                    break;
+                case 2:
+                    float a = 12.34f, b = 45.78f, c = 90.54f;
+                    Console.WriteLine("{0} is having Max value out of{1},{2},{3}", FindMaxOfFloat.FindMaxValue(a, b, c), a, b, c);
+                    break;
+                case 3:
+                    string s1 = "abc", s2 = "abc", s3 = "zbc";
+                    Console.WriteLine("{0} is having Max value out of{1},{2},{3}", FindMaxOfString.FindMaxValue(s1, s2, s3), s1, s2, s3);
+                    break;
+                case 4:
+                    int k = 15, l = 34, m = 90;
+                    new GenericsFindMax<int>(k, l, m).TestMaximum();
+                    break;
+                default:
+                    break;
+            }
             Console.ReadLine();
-        }
+        }       
     }
 }
