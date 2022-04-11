8845d7c28d7b0cecd123ef8ed90872dda7c7f4ea
duvvamounika <mounikaduvva05@gmail.com>
2022-04-11 09:27:51 +0530

    Add MaxOfIntegerUC1 branch and program

--- Generics-Day13/Program.cs
+++ Generics-Day13/Program.cs
@@ -11,6 +11,8 @@ namespace Generics_Day13
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to generics");
+            int x = 10, y = 20, z = 30;
+            Console.WriteLine("{0} is having Max value out of{1},{2},{3}", FindMaxOfInteger.FindMaxValue(x, y, z), x, y, z);
             Console.ReadLine();
         }
     }
